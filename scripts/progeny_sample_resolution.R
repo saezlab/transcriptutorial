a78d84320786e6b85db0e42965c9b6c154d284ee
Aurelien Dugourd <Aurelien@Aureliens-Macbook.local>
2020-07-27 11:43:02 +0200

    progeny sample resolution

---
+++ scripts/progeny_sample_resolution.R
@@ -0,0 +1,21 @@
+library(readr)
+library(viper)
+library(progeny)
+
+working_dir <- "~/Documents/transcriptutorial/" #put whatever is your working directory here
+setwd(working_dir)
+
+
+source("scripts/support_functions.R")
+
+count_df_vsn <- as.data.frame(read_csv("data/count_df_vsn.csv"))
+row.names(count_df_vsn) <- count_df_vsn$gene
+
+count_df_vsn <- count_df_vsn[,-1]
+count_df_vsn <- count_df_vsn[complete.cases(count_df_vsn),]
+
+PathwayActivity_counts <- progeny(as.matrix(count_df_vsn), scale=TRUE, 
+                                  organism="Human", top = 100, perm = 10000, z_scores = T)
+PathwayActivity_counts <- as.data.frame(t(PathwayActivity_counts))
+
+write.csv(PathwayActivity_counts,"~/Documents/transcriptutorial/data/progeny_sample_resolution.csv")
