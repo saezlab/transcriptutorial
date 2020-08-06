4e2c3a4b7b35e8642584ac0f8b54b39c0b1328c9
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2020-08-06 14:28:16 +0200

    changed zscores as False

--- scripts/progeny_sample_resolution.R
+++ scripts/progeny_sample_resolution.R
@@ -2,7 +2,7 @@ library(readr)
 library(viper)
 library(progeny)
 
-working_dir <- "~/Documents/transcriptutorial/" #put whatever is your working directory here
+working_dir <- "~/Projects/transcriptutorial/" #put whatever is your working directory here
 setwd(working_dir)
 
 
@@ -15,7 +15,7 @@ count_df_vsn <- count_df_vsn[,-1]
 count_df_vsn <- count_df_vsn[complete.cases(count_df_vsn),]
 
 PathwayActivity_counts <- progeny(as.matrix(count_df_vsn), scale=TRUE, 
-                                  organism="Human", top = 100, perm = 10000, z_scores = T)
+                                  organism="Human", top = 100, perm = 10000, z_scores = F)
 PathwayActivity_counts <- as.data.frame(t(PathwayActivity_counts))
 
-write.csv(PathwayActivity_counts,"~/Documents/transcriptutorial/data/progeny_sample_resolution.csv")
+write.csv(PathwayActivity_counts,"~/Projects/transcriptutorial/data/progeny_sample_resolution.csv")
